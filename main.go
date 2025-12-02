1251b7f68cbb897a1a4d439e64ac2ed0532dd6cf
Georgi Kanchev <gogata.199999@gmail.com>
2025-12-02 23:14:00 +0200

    Updated engine; Deleted old backup note files

--- main.go
+++ main.go
@@ -2,43 +2,28 @@ package main
 
 import (
 	"pure-game-kit/data/assets"
-	"pure-game-kit/geometry"
 	"pure-game-kit/graphics"
 	"pure-game-kit/input/keyboard"
 	"pure-game-kit/input/keyboard/key"
-	"pure-game-kit/tiled/tilemap"
-	"pure-game-kit/utility/color"
+	"pure-game-kit/tiled"
 	"pure-game-kit/window"
 )
 
-var maps []*graphics.Sprite
-var shapes []*geometry.Shape
-
 func main() {
 	var camera = graphics.NewCamera(1)
-	reload()
+	var assetId = assets.LoadTiledMap("maps/test/map.tmx")
+	var projectId = assets.LoadTiledProject("maps/map.tiled-project")
+	var project = tiled.NewProject(projectId)
+	var mapp = tiled.NewMap(assetId, project)
 
 	for window.KeepOpen() {
 		camera.SetScreenAreaToWindow()
-		camera.DrawSprites(maps...)
+		camera.MouseDragAndZoomSmooth()
 
-		camera.DrawCircle(0, 0, 100, color.Red)
-
-		for _, s := range shapes {
-			var pts = s.CornerPoints()
-			camera.DrawShapes(color.FadeOut(color.Red, 0.65), pts...)
-			camera.DrawLinesPath(2, color.Red, pts...)
-		}
-
-		camera.DragAndZoom()
 		if keyboard.IsKeyJustPressed(key.F5) {
-			reload()
+			assets.ReloadAll()
 		}
-	}
-}
 
-func reload() {
-	assets.LoadTiledMap("maps/test/map.tmx")
-	maps = tilemap.LayerSprites("maps/test/map.tmx", "Background", "")
-	shapes = tilemap.LayerShapes("maps/test/map.tmx", "Zones", "")
+		mapp.Draw(camera)
+	}
 }
