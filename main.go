d9401a5e05e22db19dcab772b1aff7c68e878cec
Georgi Kanchev <gogata.199999@gmail.com>
2025-12-04 00:24:23 +0200

    0.0.3 Inventory - first sweep outline

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"pure-game-kit/data/assets"
+	"pure-game-kit/execution/condition"
 	"pure-game-kit/graphics"
 	"pure-game-kit/gui"
 	d "pure-game-kit/gui/dynamic"
@@ -24,40 +25,9 @@ func main() {
 	var projectId = assets.LoadTiledProject("project.tiled-project")
 	var project = tiled.NewProject(projectId)
 	var mapp = tiled.NewMap(assetId, project)
+	var worldHud = initWorldHud()
 
-	var _, iconIds = assets.LoadDefaultAtlasIcons()
-
-	var worldHud = gui.NewElements(
-		gui.Container("themes", "", "", "", ""),
-		gui.Theme("panel", f.Color, "43 33 23 255", f.FrameColor, "86 66 46 255", f.FrameSize, "-5",
-			f.TextLineHeight, "25", f.TextAlignmentX, "0.5", f.TextAlignmentY, "0.5"),
-		gui.Theme("resource", f.TextLineHeight, "20", f.Width, "80", f.Height, "40",
-			f.TextAlignmentX, "0.5", f.TextAlignmentY, "0.5", f.TextLineGap, "-1"),
-		//=================================================================
-		gui.Container("resources", d.CameraLeftX+"+5", d.CameraTopY+"+5", "400", "40"),
-		gui.Visual("resources-background", f.ThemeId, "panel", f.FillContainer, ""),
-		gui.Visual("gold", f.ThemeId, "resource", f.Text, "^^ 90 523", f.TextEmbeddedAssetId1, iconIds[1]),
-		gui.Visual("food", f.ThemeId, "resource", f.Text, "^^ 188", f.TextEmbeddedAssetId1, iconIds[2]),
-		gui.Visual("tools", f.ThemeId, "resource", f.Text, "^^ 77", f.TextEmbeddedAssetId1, iconIds[3]),
-		gui.Visual("ammo", f.ThemeId, "resource", f.Text, "^^ 68", f.TextEmbeddedAssetId1, iconIds[4]),
-		gui.Visual("medicine", f.ThemeId, "resource", f.Text, "^^ 29", f.TextEmbeddedAssetId1, iconIds[5]),
-		//=================================================================
-		gui.Container("goal", d.TargetLeftX, d.TargetBottomY, d.TargetWidth, "40", f.TargetId, "resources"),
-		gui.Visual("goal-background", f.ThemeId, "panel", f.FillContainer, "", f.Text, "goal"),
-		//=================================================================
-		gui.Container("time", d.CameraCenterX+"-50", d.CameraTopY+"+5", "80", "80"),
-		gui.Visual("time-background", f.ThemeId, "panel", f.FillContainer, "", f.Text, "day 16",
-			f.TextAlignmentY, "0"),
-		//=================================================================
-		gui.Container("menus", d.CameraRightX+"-405", d.CameraTopY+"+5", "400", "40"),
-		gui.Visual("menus-background", f.ThemeId, "panel", f.FillContainer, ""),
-		//=================================================================
-		gui.Container("quest", d.TargetLeftX, d.TargetBottomY, d.TargetWidth, "40", f.TargetId, "menus"),
-		gui.Visual("quest-background", f.ThemeId, "panel", f.FillContainer, "", f.Text, "quest"),
-	)
-
-	worldHud.Scale = 2
-
+	assets.LoadDefaultAtlasIcons()
 	assets.LoadDefaultFont()
 	assets.LoadDefaultTexture()
 
@@ -65,9 +35,7 @@ func main() {
 
 	for window.KeepOpen() {
 		camera.SetScreenAreaToWindow()
-		camera.Zoom *= 1 + 0.001*mouse.ScrollSmooth()
-		camera.Zoom = number.Limit(camera.Zoom, 0.1, 8)
-
+		
 		//=================================================================
 		// map
 
@@ -85,7 +53,17 @@ func main() {
 
 		camera.DrawTexture("", x-15, y-15, 30, 30, 0, color.Cyan)
 
+		if keyboard.IsKeyJustPressed(key.I) {
+			var hidden = condition.If(worldHud.Field("inventory", f.Hidden) == "", "1", "")
+			worldHud.SetField("popup-dim", f.Hidden, hidden)
+			worldHud.SetField("inventory", f.Hidden, hidden)
+			time.SetScale(condition.If(hidden == "1", float32(1), 0))
+		}
+
 		if !worldHud.IsAnyHovered(camera) {
+			camera.Zoom *= 1 + 0.001*mouse.ScrollSmooth()
+			camera.Zoom = number.Limit(camera.Zoom, 0.1, 8)
+
 			if mouse.IsButtonPressed(button.Left) {
 				worldHud.IsAnyHovered(camera)
 				moveTargetX, moveTargetY = camera.MousePosition()
@@ -96,6 +74,72 @@ func main() {
 			}
 		}
 
+		//=================================================================
+		// gui
+
 		worldHud.UpdateAndDraw(camera)
 	}
+}
+
+func initWorldHud() *gui.GUI {
+	var result = gui.NewFromXML(gui.NewElementsXML(
+		gui.Container("themes", "", "", "", ""),
+		gui.Theme("panel", f.Color, "43 33 23 255", f.FrameColor, "86 66 46 255", f.FrameSize, "-5",
+			f.TextLineHeight, "25", f.TextAlignmentX, "0.5", f.TextAlignmentY, "0.5"),
+		gui.Theme("resource", f.TextLineHeight, "20", f.Width, "80", f.Height, "40",
+			f.TextAlignmentX, "0.5", f.TextAlignmentY, "0.5", f.TextLineGap, "-1"),
+
+		//=================================================================
+		// top-left
+		gui.Container("resources", d.CameraLeftX+"+5", d.CameraTopY+"+5", "400", "40"),
+		gui.Visual("resources-bgr", f.ThemeId, "panel", f.FillContainer, ""),
+		gui.Visual("gold", f.ThemeId, "resource", f.Text, "^^ 90 523", f.TextEmbeddedAssetId1, "@coins3"),
+		gui.Visual("food", f.ThemeId, "resource", f.Text, "^^ 188", f.TextEmbeddedAssetId1, "@apple"),
+		gui.Visual("tools", f.ThemeId, "resource", f.Text, "^^ 77", f.TextEmbeddedAssetId1, "@ingot"),
+		gui.Visual("ammo", f.ThemeId, "resource", f.Text, "^^ 68", f.TextEmbeddedAssetId1, "@bow"),
+		gui.Visual("medicine", f.ThemeId, "resource", f.Text, "^^ 29", f.TextEmbeddedAssetId1, "@heart"),
+		//=================================================================
+		gui.Container("goal", d.TargetLeftX, d.TargetBottomY, d.TargetWidth, "40", f.TargetId, "resources"),
+		gui.Visual("goal-bgr", f.ThemeId, "panel", f.FillContainer, "", f.Text, "goal"),
+
+		//=================================================================
+		// top
+		gui.Container("time", d.CameraCenterX+"-50", d.CameraTopY+"+5", "80", "80"),
+		gui.Visual("time-bgr", f.ThemeId, "panel", f.FillContainer, "", f.Text, "day 16", f.TextAlignmentY, "0"),
+
+		//=================================================================
+		// top-right
+		gui.Container("menus", d.CameraRightX+"-405", d.CameraTopY+"+5", "400", "40"),
+		gui.Visual("menus-bgr", f.ThemeId, "panel", f.FillContainer, ""),
+		//=================================================================
+		gui.Container("quest", d.TargetLeftX, d.TargetBottomY, d.TargetWidth, "40", f.TargetId, "menus"),
+		gui.Visual("quest-bgr", f.ThemeId, "panel", f.FillContainer, "", f.Text, "quest"),
+
+		//=================================================================
+		// popups
+		gui.Container("popup-dim", d.CameraLeftX, d.CameraTopY, d.CameraWidth, d.CameraHeight, f.Hidden, "1"),
+		gui.Visual("popup-dim-bgr", f.FillContainer, "", f.Color, "0 0 0 150"),
+
+		//=================================================================
+		// inventory
+		gui.Container("inventory", d.CameraCenterX+"-500", d.CameraCenterY+"-320", "1000", "700", f.Hidden, "1"),
+		//=================================================================
+		gui.Container("equipment", d.TargetLeftX, d.TargetTopY, "280", "450", f.TargetId, "inventory",
+			f.Hidden, d.TargetHidden),
+		gui.Visual("equipment-bgr", f.ThemeId, "panel", f.FillContainer, ""),
+		//=================================================================
+		gui.Container("stash", d.TargetLeftX+"+280", d.TargetTopY+"+100", "440", "350", f.TargetId, "inventory",
+			f.Hidden, d.TargetHidden),
+		gui.Visual("stash-bgr", f.ThemeId, "panel", f.FillContainer, ""),
+		//=================================================================
+		gui.Container("perks", d.TargetRightX+"-280", d.TargetTopY, "280", "450", f.TargetId, "inventory",
+			f.Hidden, d.TargetHidden),
+		gui.Visual("perks-bgr", f.ThemeId, "panel", f.FillContainer, ""),
+		//=================================================================
+		gui.Container("units", d.TargetLeftX, d.TargetBottomY+"-250", "1000", "250", f.TargetId, "inventory",
+			f.Hidden, d.TargetHidden),
+		gui.Visual("units-bgr", f.ThemeId, "panel", f.FillContainer, ""),
+	))
+	result.Scale = 2
+	return result
 }
