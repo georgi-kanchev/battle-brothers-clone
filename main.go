6c2d19872290b01ef78679bd3d5a2ec0a47e50a8
Georgi Kanchev <gogata.199999@gmail.com>
2025-12-06 22:57:37 +0200

    Moved tiled project file in data

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 	assets.LoadDefaultFont()
 	assets.LoadDefaultTexture()
 
-	global.Project = tiled.NewProject(assets.LoadTiledProject("project.tiled-project"))
+	global.Project = tiled.NewProject(assets.LoadTiledProject("data/project.tiled-project"))
 	// global.ScreenMenu = screens.Add(nil, false)
 	global.ScreenWorld = screens.Add(world.New("data/worlds/test/map.tmx", "data/gui/world.xml"), true)
 	global.ScreenBattle = screens.Add(battle.New("data/battlegrounds/test/map.tmx", "data/gui/battle.xml"), true)
